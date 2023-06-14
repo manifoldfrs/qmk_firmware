943ff20d426af228a9cda49e01fba600edead944
Faris <faris@duck.com>
2023-06-13 21:22:26 -0700

    adjust layout for lulu

--- keyboards/boardsource/lulu/keymaps/manifoldfrs/keymap.c
+++ keyboards/boardsource/lulu/keymaps/manifoldfrs/keymap.c
@@ -16,6 +16,9 @@ enum layers {
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+//aliases for clarity in layering
+#define BSCTL   LCTL_T(KC_BSPC) // Ctrl when held, Backspace when tapped
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
@@ -28,38 +31,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCTL | LAlt |LGUI | /Space  /       \Enter \  |BackSP| RGUI | SYM  |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   | CAPS | LAlt | LGUI | /Space  /       \LCTL/ \  |Enter | RGUI | SYM  |
+ *                   |      |      |      |/       /         \ BS   \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_ESC, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                          KC_LCTL,    KC_LALT, KC_LGUI, KC_SPC,  KC_ENT,   KC_BSPC,   KC_RGUI, SYM
+                          KC_CAPS,    KC_LALT, KC_LGUI, KC_SPC,  BSCTL,  KC_ENT,   KC_RGUI, SYM
 ),
 /* Symbol Layer
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |   +  |
+ * |  ~   |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |   \| |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  ~   |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
- * |------+------+------+------+------+------|Vol Up |    |Vol Dn |------+------+------+------+------+------|
- * |  -   |  =   |  [   |   ]  | Left |Right |-------|    |-------| Down | Up   |   {  |   }  |   \  |   _  |
+ * |      | Vol- | Vol+ | Mute |      |      |-------|    |-------| Left | Down |  Up  |Right |      |      |
+ * |------+------+------+------+------+------|  {    |    |  }    |------+------+------+------+------+------|
+ * |LShift|      |  S+  |  S-  |   B+ |  B-  |-------|    |-------| PGUP | PGDN | HOME |  END |      |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |RGBTOG| HOME | END  | /Bri+   /       \Bri-  \  | PGUP |  PGDN|      |
+ *                   |RGBTOG|  MOD | MODR | /  Bri+ /       \ Bri- \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_SYMBOL] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                    KC_6   , KC_7   , KC_8   , KC_9  ,  KC_0   , KC_PLUS,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_LEFT, KC_RIGHT, KC_VOLU,  KC_VOLD, KC_DOWN, KC_UP, KC_LCBR, KC_RCBR, KC_BSLS, KC_UNDS,
-                            RGB_TOG, KC_HOME, KC_END, KC_BRIU,    KC_BRID, KC_PGUP, KC_PGDN, _______
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
+  _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_DLR,  KC_PERC,                    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_RPRN, _______,
+  KC_LSFT, _______, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_LCBR,  KC_RCBR, KC_DOWN, KC_UP, KC_HOME, KC_END, _______, KC_RSFT,
+                            RGB_TOG, RGB_MOD, RGB_RMOD, KC_BRIU,    KC_BRID, _______, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
