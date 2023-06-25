f8e93895086bad633caf82c2d5878c4ebab50dfc
Faris <faris@duck.com>
2023-06-24 18:32:13 -0700

    fix typos

--- keyboards/boardsource/lulu/keymaps/manifoldfrs/keymap.c
+++ keyboards/boardsource/lulu/keymaps/manifoldfrs/keymap.c
@@ -60,7 +60,7 @@ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,
 KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
 KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5,                                  KC_MINS, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC, KC_BSLS,
 SftCaps, KC_6, KC_7, KC_8, KC_9, KC_0,           XXXXXXX, XXXXXXX,     KC_UNDS, KC_PLUS, KC_TILD, KC_LCBR, KC_RCBR, KC_PIPE,
-                             _______, _______, KC_LTCL, KC_SPC, KC_ENT,  KC_RGUI, _______, _______
+                             _______, _______, KC_LCTL, KC_SPC, KC_ENT,  KC_RGUI, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -79,7 +79,7 @@ SftCaps, KC_6, KC_7, KC_8, KC_9, KC_0,           XXXXXXX, XXXXXXX,     KC_UNDS, 
  [_RAISE] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  XXXXXXX, KC_MPREV, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY,                  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, KC_PGUP,
+  XXXXXXX, KC_MPRV, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY,                  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, KC_PGUP,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END, KC_HOME, XXXXXXX, XXXXXXX, KC_PGDN,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
